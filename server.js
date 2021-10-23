7dae1f67e3a8dd21632803f5eaa848255449fde2
Ghidorah <jljohnson1454@gmail.com>
2021-10-22 20:45:50 -0500

    got the json response

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const notes = require('./db/db.json')
+
 
 app.get('/api/notes', (req, res) => {
-    res.send('Hello!');
+    res.json(notes);
   });
 
 
