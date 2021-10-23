d744efcea0d48e37e1f0bb61eb8611add2d2bf1a
Ghidorah <jljohnson1454@gmail.com>
2021-10-23 09:13:34 -0500

    able to find notes by title

---
+++ routes/routes.js
@@ -0,0 +1,14 @@
+// const express = require('express');
+
+
+// const app = express();
+
+// const notes = require('../db/db.json');
+
+
+
+// app.get('/api/notes', (req, res) => {
+//     res.json(notes);
+//   });
+
+//   module.exports = router;
--- server.js
+++ server.js
@@ -2,16 +2,32 @@ const express = require('express');
 const path = require('path');
 
 const notes = require('./db/db.json');
+const routes = require('./routes/routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.static('public'));
+
+function findByQuery(title, notes) {
+    const result = notes.filter(note => note.title === title)[0];
+    return result;
+  }
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
   });
 
+app.get('/api/notes/:title', (req, res) => {
+    const result = findByQuery(req.params.title, notes);
+    res.json(result);
+  });
 
+
+
+  // app.use('/api', routes);
 
 
 
