641c292d23a59ca100b4871b311811f44bce0fd2
Ghidorah <jljohnson1454@gmail.com>
2021-10-22 20:23:59 -0500

    Added Server JS file

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+
+
+
+
+
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
