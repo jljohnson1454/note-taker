d1f605aa2755b4c1bfe68306aa8b2edf813945b0
Ghidorah <jljohnson1454@gmail.com>
2021-10-23 19:04:19 -0500

    posting and iterating id is working

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+
 
 const notes = require('./db/db.json');
 const routes = require('./routes/routes');
@@ -7,24 +9,83 @@ const routes = require('./routes/routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-// app.use(express.static('public'));
+class object {
+    constructor(id, title, text){
+        this.id = id;
+        this.title = title;
+        this.text = text;
+    }
+};
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Function to find note by title
 function findByQuery(title, notes) {
     const result = notes.filter(note => note.title === title)[0];
     return result;
   }
 
+// GET requests to find notes
 app.get('/api/notes', (req, res) => {
     res.json(notes);
   });
 
 app.get('/api/notes/:title', (req, res) => {
-    const result = findByQuery(req.params.title, notes);
+    const result = findByQuery(req.query.title, notes);
     res.json(result);
   });
 
+// app.get('/api/query', (req, res) => {
+//     res.json(req.query);
+// });
+
+
+// POST routes to add new notes
+let globalData;
+const writeNote = (body) => {
+    
+    fs.readFile('./db/db.json', 'utf-8', function (err, data) {
+        globalData = data;
+        console.log(data);
+        notes.push(body);
+    
+        fs.writeFile('./db/db.json', JSON.stringify(notes), () =>{console.log('fileWritten')})
+    })
+};
+
+
+
+
+app.post('/api/notes', (req, res) => {
+    // req.body is where our incoming content will be
+    //req.body.id = notes.length;
+    let reqBody = req.body;
+    
+    let body = new object(reqBody.id, reqBody.title, reqBody.text);
+    console.log(body);
+    writeNote(body);
+    // fs.readFile('./db/db.json','utf-8', (err, data) => {
+    //     console.log(data);
+    //     globalData = data;
+    //     console.log('working')})
+    
+    res.json(globalData);
+    
+  });
+
+
+
+// app.post('/api/notes', (req, res) => {
+//     const newNote = req.body;
+  
+//     console.log(newNote);
+  
+//     notes.push(newNote);
+  
+//     res.json(newNote);
+//   });
 
 
   // app.use('/api', routes);
